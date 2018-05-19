10c33404696cf17a1a9cb7373f63ef20428cb1c2
MelanieSharon <mell.250697@gmail.com>
2018-05-19 14:52:59 -0500

    tipos de declaraciones

---
+++ Main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var _name string = "Melanie"
+	_nameTwo := "Sharon"
+
+	var (
+		_nameThree string
+	)
+	_nameThree = "Veliz Ruiz"
+
+	fmt.Println(_name, _nameTwo, _nameThree)
+}
