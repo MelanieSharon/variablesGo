1aa12097c798ea8e9faef52b7fb490c691bf8bb9
MelanieSharon <mell.250697@gmail.com>
2018-05-19 15:29:47 -0500

    Tipos de variables

--- Main.go
+++ Main.go
@@ -14,4 +14,20 @@ func main() {
 	_nameThree = "Veliz Ruiz"
 
 	fmt.Println(_name, _nameTwo, _nameThree)
+	fmt.Println("---------------------------")
+
+	var (
+		_valeInt     int
+		_valueFloat  float64
+		_valueString string
+		_valueBool   bool
+	)
+
+	_valeInt = 20
+	_valueFloat = 50.456
+	_valueBool = false
+	_valueString = "Leslie"
+
+	fmt.Println(_valeInt, _valueFloat, _valueBool, _valueString)
+
 }
